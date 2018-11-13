03349377a33e070ae210214ccea42c5f569e4986
Jakub Pasula <kuba10pl@gmail.com>
2018-11-13 19:42:36 +0100

    Add files via upload

---
+++ LunarLogicRecruitmentTask V1.js
@@ -0,0 +1,74 @@
+﻿var tmp = [];
+var occurRate = [];
+
+function setup() {
+    alter = createInput('AABBCCCCBDDDADB', 'text');
+    input = alter.value();
+    alter.style('font-size', '30px');
+    alter.changed(setInput);
+    createCanvas(800, 250);
+    fill(255);
+    textSize(30);
+    frameRate(1);
+}
+
+function draw() {
+    background(220);
+    background(0);
+    solve();
+}
+
+
+function setInput() {
+    createCanvas(800, 250);
+    fill(255);
+    textSize(30)
+    input = alter.value();
+}
+
+
+
+function solve() {
+    var tmp = [];
+    var occurRate = [];
+    text("↑↑Click title for my solution↑↑", 20, 40);
+    text("For new input. Alter input box and press enter.", 20, 100);
+    text(input, 20, 180);
+
+    //this for loop counts occur rates of the same letter in a row
+    //and creates array of it
+    for (var i = 0; i < input.length; i++) {
+        this.count = 0;
+        if (input[i] === input[i + 1]) {
+            j = i;
+            this.letter = input[i];
+            for (j; j < input.length; j++) {
+                if (this.letter == input[j]) this.count += 1;
+                if (this.letter != input[j]) break;
+            }
+            occurRate = append(occurRate, this.count);
+            i = i + this.count - 1;
+        } else {
+            occurRate = append(occurRate, 1);
+        }
+    }
+    j = 0;
+    var k = 0;
+    //this loop looks for occur Rate of letters if its smallet than 3 then it is added to tmp array
+    //otherwise it skips increses index in appending loop that rewrites array
+    //so it skips the letters of occurRate that is 3 or greater :D
+    for (var i = 0; i < occurRate.length; i++) {
+        if (occurRate[i] < 3) {
+            j = j + occurRate[i];
+            for (k; k < j; k++) tmp = append(tmp, input[k]);
+            k = j;
+        }
+        if (occurRate[i] > 2) {
+            j = j + occurRate[i];
+            k = j;
+        }
+
+    }
+    input = tmp;
+    if (max(occurRate) < 2) input = alter.value();
+}
---
+++ LunarLogicRecruitmentTaskV2.js
@@ -0,0 +1,60 @@
+﻿var tmp = [];
+
+function setup() {
+    alter = createInput('AABBCCCCBDDDADB', 'text');
+    input = alter.value();
+    alter.style('font-size', '30px');
+    alter.changed(setInput);
+    createCanvas(800, 250);
+    fill(255);
+    textSize(30);
+    frameRate(1);
+}
+
+function draw() {
+    background(220);
+    background(0);
+    solve();
+}
+
+
+function setInput() {
+    createCanvas(800, 250);
+    fill(255);
+    textSize(30)
+    input = alter.value();
+}
+
+function solve() {
+    var tmp = [];
+    text("↑↑Click title for my solution↑↑", 20, 40);
+    text("For new input. Alter input box and press enter.", 20, 100);
+    text(input, 20, 180);
+    this.count = 0;
+    this.letter = input[0];
+    //this loop just rewrites input to tmp array
+    for (var i = 0; i < input.length; i++) {
+        this.letter = input[i]
+        if (this.letter === input[i]) {
+            this.count = 0;
+            this.j = i;
+            for (this.j; this.j < input.length; this.j++) {
+                if (this.letter === input[this.j]) {
+                    this.count += 1;
+                } else {
+                    if (this.count > 2) i = this.j;
+                    break;
+                }
+            }
+        }
+
+        if (this.j === input.length && this.count === this.j) {
+            tmp = [];
+            break;
+        } else {
+            tmp = append(tmp, input[i]);
+        }
+
+    }
+    input = tmp;
+}
